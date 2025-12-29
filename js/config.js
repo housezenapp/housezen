383e5b7fea4780591dea7a64ee86e0034332f729
housezenapp <housezen.app@gmail.com>
2025-12-29 13:23:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = 'https://rplieisbxvruijvnxbya.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_bwA6-ahysrqAjTYZsgbzVw_PbZ7-BQR';
+const SUPABASE_URL = 'https://ebkubuxrzgmenmcjyima.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_6fp2CquDO9YZlBrT4jVa9Q_q4hd7AzA';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 let currentUser = null;
